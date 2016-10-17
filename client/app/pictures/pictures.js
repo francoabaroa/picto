e59ab11266f5b635cc72c5ce8d6b2161fcc5c918
Franco Abaroa <franco9395@gmail.com>
2016-10-17 00:26:00 -0700

    Modified request object so POST request could work

--- client/app/pictures/pictures.js
+++ client/app/pictures/pictures.js
@@ -1,5 +1,23 @@
 angular.module('picto', [])
-  .controller('pictureController', function ($scope) {
+  .factory('Pics', function ($http) {
+    return {
+      addOne: function(pic) {
+        console.log('in add one!!');
+        return $http({
+          method: 'POST',
+          url: '/pics',
+          data: {name: pic.name, url: pic.url, rating: pic.rating}
+        }).then(function (resp) {
+          console.log('pictures.js', resp.data);
+          return resp.data;
+        })
+        .catch(function (err) {
+          return err;
+        });
+      }
+    };
+  })
+  .controller('pictureController', function ($scope, Pics) {
     $scope.indices = {Paco:0, Flaco:1, Taco:2, Caco:3};
     $scope.obj = [{url: 'https://pbs.twimg.com/profile_images/737359467742912512/t_pzvyZZ_400x400.jpg' , title: 'Paco', rating: 3}, {url: 'https://pbs.twimg.com/profile_images/625769159339737088/2dwpQAXA.jpg' , title: 'Flaco', rating: 4}, {url: 'https://pbs.twimg.com/profile_images/630664501776527361/nIK2xTUE.jpg' , title: 'Taco', rating: 4}, {url: 'https://pbs.twimg.com/profile_images/659017415339220992/0QxomPDQ.jpg' , title: 'Caco', rating: 5} ];
     $scope.names = ['Paco', 'Flaco', 'Taco', 'Caco'];
@@ -19,11 +37,21 @@ angular.module('picto', [])
     }
 
     $scope.catUpload = function () {
-      $scope.obj.push({url: $scope.catLink, title: $scope.catName, rating: 0});
-      $scope.names.push($scope.catName);
-      $scope.currInd++;
-      $scope.indices[$scope.catName] = $scope.currInd;
-      $scope.catLink = '';
-      $scope.catName = '';
+      console.log('picLINK:', $scope.picLink);
+      var url = $scope.picLink;
+      Pics.addOne({name: $scope.picName, rating: $scope.picRating, url: url})
+      .then(function (pic) {
+        console.log(pic, 'catUpload pic');
+      })
+      .catch(function (err) {
+        console.log(err);
+      });
+
+      // $scope.obj.push({url: $scope.catLink, title: $scope.catName, rating: 0});
+      // $scope.names.push($scope.catName);
+      // $scope.currInd++;
+      // $scope.indices[$scope.catName] = $scope.currInd;
+      // $scope.catLink = '';
+      // $scope.catName = '';
     }
   });
