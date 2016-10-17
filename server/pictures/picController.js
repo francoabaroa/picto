03e05c89bc8c165cec0b740cf58e1d1491a2766b
Franco Abaroa <franco9395@gmail.com>
2016-10-17 00:23:33 -0700

    Added a method to add new picture to the model

--- server/pictures/picController.js
+++ server/pictures/picController.js
@@ -1,1 +1,26 @@
 var Pic = require('./picModel.js');
+var Q = require('q');
+
+// Promisify a few mongoose methods with the `q` promise library
+var findPic = Q.nbind(Pic.findOne, Pic);
+var createPic = Q.nbind(Pic.create, Pic);
+var findAllPics = Q.nbind(Pic.find, Pic);
+
+module.exports = {
+  newPic: function (req, res, next) {
+    var url = req.body.url;
+    var name = req.body.name;
+    var rating = req.body.rating;
+    console.log('in new PICS!!');
+    console.log('req.body: ', req.body);
+    createPic({name: name, url: url, rating: rating})
+    .then(function (pic) {
+      if (pic) {
+        res.json(pic);
+      }
+    })
+    .fail(function (err) {
+      next(err);
+    })
+  }
+};
